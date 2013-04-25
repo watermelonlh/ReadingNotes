7b02eb842b97e428d5c81c8b266ad5159a9221b7
watermelon <watermelonlh@gmail.com>
2013-04-26 03:17:16 +0800

    scan repo

--- ScanRepo.py
+++ ScanRepo.py
@@ -3,6 +3,23 @@
 import glob
 import os
 
-os.chdir("repo")
-for files in glob.glob("*.html"):
-    print "<h3><a class='note' href='repo/%s' >%s</a></h3>" % (files, files[:files.find('.')])
+f1 = open("template", "r")
+f = f1.read()
+f1.close()
+
+section = ""
+for files in glob.glob("repo/*.html"):
+    text = "<h3><a class='note' href='repo/%s' >%s</a></h3>\n" % (files, files[:files.find('.')])
+    section = section + text
+
+nf = f.replace("$section$", section)
+
+try:
+    f2 = open("index.html", "w")
+    f2.write(nf)
+    f2.flush()
+    f2.close()
+except IOError as (e, stre):
+    print e, stre
+
+#print nf
