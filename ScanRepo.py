2c86785cdcbcc0b0e550d4846fad7c5807037670
watermelon <watermelonlh@gmail.com>
2013-04-26 01:33:55 +0800

    update

--- ScanRepo.py
+++ ScanRepo.py
@@ -5,4 +5,4 @@ import os
 
 os.chdir("repo")
 for files in glob.glob("*.html"):
-    print "<h3><a href='repo/%s' >%s</a></h3>" % (files, files[:files.find('.')])
+    print "<h3><a class='note' href='repo/%s' >%s</a></h3>" % (files, files[:files.find('.')])
